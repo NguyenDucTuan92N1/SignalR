7f280e906701d40c475d1235a29b0b4b0e649d66
NguyenDucTuan92N1 <64392816+NguyenDucTuan92N1@users.noreply.github.com>
2021-11-11 17:23:48 +0700

    first commit

---
+++ WebApplication2/Hubs/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Hubs
+{
+
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+    public class ContosoChatHub : Hub
+    {
+        public async Task JoinRoom(string roomName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+            await Clients.Group(roomName).SendAsync(Context.User.Identity.Name + " joined.");
+
+        }
+
+        public Task LeaveRoom(string roomName)
+        {
+            return Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+        }
+    }
+}
